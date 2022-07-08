df3e5cd1e5c7cd29b660f4ccd4ef0d9595893b4a
Jose Freitas <pg42872@alunos.uminho.pt>
2022-07-08 13:59:39 +0100

    CI debug

--- ci/ci_build.sh
+++ ci/ci_build.sh
@@ -1,4 +1,8 @@
 PREFIX="/opt/conda"
 mkdir -p "${PREFIX}/bin"
-cp PlastEDMA/workflow/plastedma.py PlastEDMA/resources/* "${PREFIX}/bin"
+cp PlastEDMA/workflow/plastedma.py "${PREFIX}/bin"
+cp -r PlastEDMA/resources/* "${PREFIX}/bin"
+cp -r PlastEDMA/workflow/scripts "${PREFIX}/bin"
+cp -r PlastEDMA/results "${PREFIX}/bin"
+cp -r PlastEDMA/config "${PREFIX}/bin"
 chmod +x /opt/conda/bin/plastedma.py
