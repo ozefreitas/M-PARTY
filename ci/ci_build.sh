80ce10ef0c69ff8baca6c657a65ba6ae94ebef2e
Jose Freitas <pg42872@alunos.uminho.pt>
2022-07-08 14:58:27 +0100

    Please be right

--- ci/ci_build.sh
+++ ci/ci_build.sh
@@ -1,8 +1,9 @@
 PREFIX="/opt/conda"
 mkdir -p "${PREFIX}/bin"
-cp PlastEDMA/workflow/plastedma.py "${PREFIX}/bin"
-cp -r PlastEDMA/resources/* "${PREFIX}/bin"
-cp -r PlastEDMA/workflow/scripts "${PREFIX}/bin"
+mkdir -p "${PREFIX}/bin/workflow"
+cp PlastEDMA/workflow/plastedma.py "${PREFIX}/bin/workflow"
+cp -r PlastEDMA/workflow/scripts "${PREFIX}/bin/workflow"
+cp -r PlastEDMA/resources "${PREFIX}/bin"
 cp -r PlastEDMA/results "${PREFIX}/bin"
 cp -r PlastEDMA/config "${PREFIX}/bin"
 chmod +x /opt/conda/bin/plastedma.py
