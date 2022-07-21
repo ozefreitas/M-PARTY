1b94529a8837898b3002edc7046ab873df6f1196
Jose Freitas <pg42872@alunos.uminho.pt>
2022-07-21 14:43:44 +0100

    Add bioconda files

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+INSTALL_FOLDER=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
+BIN_FOLDER=$PREFIX/bin
+mkdir -p $INSTALL_FOLDER
+mkdir -p $BIN_FOLDER
+
+cp -r workflow $INSTALL_FOLDER/
+cp -r config $INSTALL_FOLDER/
+cp -r resources $INSTALL_FOLDER/
+cp -r results $INSTALL_FOLDER/
+cp plastedma.py $INSTALL_FOLDER/
+echo "Listing copied files from package to install folder"
+ls -lt $INSTALL_FOLDER
+
+ln -s $INSTALL_FOLDER/plastedma.py $BIN_FOLDER
+echo "Listing bin folder links from install folder"
+ls -l $BIN_FOLDER
+chmod +x $BIN_FOLDER/plastedma.py
